95cd345e23aa4c292d0ddb10802d27a259713745
Saurabh Dorle <35068590+saurabhdorle@users.noreply.github.com>
2018-12-31 17:25:20 +0530

    Update lstm.py

--- lstm.py
+++ lstm.py
@@ -14,12 +14,12 @@ from keras.preprocessing.text import Tokenizer
 from keras.preprocessing import sequence
 from keras.callbacks import EarlyStopping
 
-# Importing datase
+# Importing dataset
 dataset = pd.read_csv("spam.csv", encoding = "latin-1")
 dataset.head()
 
 
-'''As the preview of the data above shows there are three useless columns, 
+'''As the preview of the data shows there are three useless columns, 
 these should be removed. I will also rename the remaining columns as "label" and "text" are not descriptive'''
 
 dataset = dataset.drop(["Unnamed: 2", "Unnamed: 3", "Unnamed: 4"], axis=1)
@@ -40,7 +40,7 @@ le = LabelEncoder()
 y = le.fit_transform(y)
 y = y.reshape(-1, 1)
 
-# SPliting dataset into trainnig and tesing data
+# SPliting dataset into trainig and tesing data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
 
 ''' Process the data
@@ -55,19 +55,20 @@ sequences = tok.texts_to_sequences(X_train)
 sequences_matrix = sequence.pad_sequences(sequences, maxlen = max_len)
 
 # =============================================================================
-# RNN
+# RNN(LSTM)
 # =============================================================================
 
 model = Sequential()
 model.add(keras.layers.InputLayer(input_shape = [max_len]))
-model.add(keras.layers.Embedding(max_words, 50 , input_length = max_len))
+model.add(keras.layers.Embedding(max_words, 50 , input_length = max_len)) #for Word embedding (text to vector)
 model.add(keras.layers.LSTM(64))
 model.add(keras.layers.Dense(units = 256, name = 'FC1', activation='relu'))
-model.add(Dropout(0.5))
+model.add(Dropout(0.5)) #                                                 To overcome the posibility of overfitting
 model.add(keras.layers.Dense(units=1, name = 'Out_layer', activation='sigmoid'))
 
 model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
+# Fitting model to training data
 model.fit(sequences_matrix, y_train, batch_size = 128, epochs=10,
           validation_split = 0.2, callbacks = [EarlyStopping(monitor = 'val_loss', min_delta = 0.0001)])
 
